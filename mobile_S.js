4192b8fba627757613a46fc0354c99d503651d77
yoonu <yoonu78@naver.com>
2015-12-01 16:38:29 +0900

    151201

---
+++ mobile_S.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" language="JavaScript">
+		var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
+		for (var word in mobileKeyWords){
+		if (navigator.userAgent.match(mobileKeyWords[word]) != null){
+		window.location.href = "http://sudal0820.dothome.co.kr/seulkiM";
+		break;
+		}
+		}
+	</script>
