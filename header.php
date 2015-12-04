9422ad28f611a0cf42a9f0866dc3dd51fe605578
yoonu <yoonu78@naver.com>
2015-12-04 10:46:28 +0900

    151204ici

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">	
+	<link rel="stylesheet" href="css/layout.css" />
+	<link rel="stylesheet" href="css/contact.css" />
+	<link rel="shortcut icon" href="images/layout/favicon.ico">
+	<title>Contact</title>
+	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/sticker.min.js"></script>
+	<script type="text/javascript" src="js/html5shiv.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+				window.onload = function() {
+				            Sticker.init('.sticker');
+			        		}
+			        	});
+	</script>
+
+</head>
